faae3c556bb434fa35ed9515327d0fbb6cb90f20
Ran <skylab.ran@gmail.com>
2018-08-12 14:12:24 +0700

    [Update] Add tag `bson`

--- types/cryptocurrency.go
+++ types/cryptocurrency.go
@@ -3,25 +3,25 @@ package types
 import "time"
 
 type Cryptocurrency struct {
-	ID                int                 `json:"id"`
-	Name              string              `json:"name"`
-	Symbol            string              `json:"symbol"`
-	Slug              string              `json:"slug"`
-	CirculatingSupply float64             `json:"circulating_supply"`
-	TotalSupply       float64             `json:"total_supply"`
-	MaxSupply         float64             `json:"max_supply"`
-	DateAdded         time.Time           `json:"date_added"`
-	NumMarketPairs    int                 `json:"num_market_pairs"`
-	CmcRank           int                 `json:"cmc_rank"`
-	LastUpdated       time.Time           `json:"last_updated"`
-	Quote             CryptoCurrencyQuote `json:"quote"`
+	ID                int                 `json:"id" bson:"id"`
+	Name              string              `json:"name" bson:"name"`
+	Symbol            string              `json:"symbol" bson:"symbol"`
+	Slug              string              `json:"slug" bson:"slug"`
+	CirculatingSupply float64             `json:"circulating_supply" bson:"circulating_supply"`
+	TotalSupply       float64             `json:"total_supply" bson:"total_supply"`
+	MaxSupply         float64             `json:"max_supply" bson:"max_supply"`
+	DateAdded         time.Time           `json:"date_added" bson:"date_added"`
+	NumMarketPairs    int                 `json:"num_market_pairs" bson:"num_market_pairs"`
+	CmcRank           int                 `json:"cmc_rank" bson:"cmc_rank"`
+	LastUpdated       time.Time           `json:"last_updated" bson:"last_updated"`
+	Quote             CryptoCurrencyQuote `json:"quote" bson:"quote"`
 }
 
 type CryptoCurrencyQuote struct {
-	USD USD `json:"USD"`
+	USD PriceCovert `json:"USD"`
 }
 
-type USD struct {
+type PriceCovert struct {
 	Price            float64   `json:"price"`
 	Volume24H        float64   `json:"volume_24h"`
 	PercentChange1H  float64   `json:"percent_change_1h"`
