58f4f19cc3145a845a019b3b01f1ba96aee48c3e
Ran <skylab.ran@gmail.com>
2018-08-17 12:06:16 +0700

    [Updated] - Move struct StatusResponse to file status.go

--- types/cryptocurrency.go
+++ types/cryptocurrency.go
@@ -69,11 +69,3 @@ type CryptocurrencyResponse struct {
 	Status StatusResponse        `json:"status"`
 	Data   CryptocurrencyListing `json:"data"`
 }
-
-type StatusResponse struct {
-	Timestamp    time.Time `json:"timestamp"`
-	ErrorCode    int       `json:"error_code"`
-	ErrorMessage string    `json:"error_message"`
-	Elapsed      int       `json:"elapsed"`
-	CreditCount  int       `json:"credit_count"`
-}
