2ab4a798c2a8f8fb42534e3159c088ea1849400f
Ran <skylab.ran@gmail.com>
2018-09-09 14:45:33 +0700

    Updated test case 3 for Hobbyist plan

--- rate_limit_test.go
+++ rate_limit_test.go
@@ -26,6 +26,14 @@ func TestCalculateCreditLimitWithPlan(t *testing.T) {
 			args{
 				opt: &PlanOptions{PlanID: 0, Keys: []string{}}},
 			nil},
+		{"Test case 3",
+			args{
+				opt: &PlanOptions{PlanID: 1, Keys: []string{"2", "3", "4"}}},
+			&CreditLimit{
+				DailyCreditLimit:   6000,
+				MonthlyCreditLimit: 180000,
+				APICallRateLimit:   90,
+			}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
